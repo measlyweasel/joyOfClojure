b63d2db1961599f3195bc011033c9b89ef96d7bf
jason cahoon <jfcahoon@gmail.com>
2014-04-15 21:08:09 -0500

    chapter 3 done, cool graphics

--- src/chapter3.clj
+++ src/chapter3.clj
@@ -3,7 +3,7 @@
 (defn ex1 [] (for [x (range 2) y (range 2)] (bit-xor x y)))
 
 
-(defn xors [maxX maxY] (for [x (range maxX) y (range maxY)] [x y (bit-xor x y)]))
+(defn xors [maxX maxY] (for [x (range maxX) y (range maxY)] [x y (rem (bit-xor x y) 256)]))
 
 
 (def frame (java.awt.Frame.))
@@ -20,5 +20,28 @@
     method-name))
 
 
-(.setSize frame (java.awt.Dimension. 200 200))
+(.setSize frame (java.awt.Dimension. 500 500))
 (.setVisible frame true)
+
+(def gfx (.getGraphics frame))
+
+(doseq [[x y xor] (xors 500 500)]
+  (.setColor gfx (java.awt.Color. xor xor xor))
+  (.fillRect gfx x y 1 1))
+
+(defn f-values [f xs ys]
+  (for [x (range xs) y (range ys)]
+    [x y (rem (f x y) 256)]))
+
+(defn clear [g xs ys] (.clearRect g 0 0 xs ys))
+
+(defn draw-values [f xs ys]
+  (clear gfx xs ys)
+  (.setSize frame (java.awt.Dimension. xs ys))
+  (doseq [[x y v] (f-values f xs ys)]
+    (.setColor gfx (java.awt.Color. v v v))
+    (.fillRect gfx x y 1 1)))
+
+
+
+
